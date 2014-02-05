6ab7e59b4f5813378ecb4997b8b7be291581d5ba
Alan Edwardes <alan@alanedwardes.com>
2014-02-05 15:47:32 -0800

    Fixed CORS headers not being sent for services.

--- api/http.js
+++ api/http.js
@@ -5,7 +5,7 @@ exports.requestHandler = function(express, app)
     app.use(app.router);
 
     // Allow cross origin
-    app.all('/', function(req, res, next) {
+    app.all('/*', function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         next();
